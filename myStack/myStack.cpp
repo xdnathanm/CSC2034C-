d25f9f838b54529c1f25b309105e4496c059bcd6
McIntyre_Nathan <49591115+xdnathanm@users.noreply.github.com>
2023-03-01 13:58:17 -0700

    Add files via upload

---
+++ myStack/myStack.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <myStack.h>
+using namespace std;
+
+
+int main()
+{
+
+	return 0;
+
+}
+// the constructor
+// create an instence of myStack
+
+myStack :: myStack()
+{
+	
+}
+// the destructor
+myStack :: ~myStack()
+{
+						
+}
+
+
+void push(int number)
+{
+
+}
+int pop()
+{
+
+}
+bool isEmpty()
+{
+	bool condition = true;
+	return condition;
+}
+void print()
+{
+
+}
---
+++ myStack/myStack.h
@@ -0,0 +1,22 @@
+#ifndef MYSTACK_H
+#define MYSTACK_H
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class myStack {
+private:
+	vector<int> data;
+
+public:
+	myStack();
+	~myStack();
+	void push(int);
+	int pop();
+	bool isEmpty();
+	void print();
+	friend std::ostream& operator << (std::ostream&, myStack&);
+
+};
+#endif
