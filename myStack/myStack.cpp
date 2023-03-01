1483b17ec306f09cc7d9bb76448a8f6479ca90e0
McIntyre_Nathan <49591115+xdnathanm@users.noreply.github.com>
2023-03-01 14:26:26 -0700

    Add files via upload

--- myStack/myStack.cpp
+++ myStack/myStack.cpp
@@ -3,40 +3,43 @@
 using namespace std;
 
 
-int main()
-{
 
-	return 0;
-
-}
 // the constructor
 // create an instence of myStack
+class myStack {
 
-myStack :: myStack()
-{
-	
-}
-// the destructor
-myStack :: ~myStack()
-{
-						
-}
+private:
+	vector < int> data;
+public:
+	myStack()
+	{
+		vector <int> data = { 0 };
+	}
+	// the destructor
+	 ~myStack()
+	{
+		 data.clear();
+	}
 
 
-void push(int number)
-{
-
-}
-int pop()
-{
-
-}
-bool isEmpty()
-{
-	bool condition = true;
-	return condition;
-}
-void print()
-{
-
-}
+	void push(int number)
+	{
+		data.push_back(number);
+	}
+	int pop()
+	{
+		data.pop_back();
+	}
+	bool isEmpty()
+	{
+		return data.empty();
+	}
+	void print()
+	{
+		cout << " Info from vector: ";
+		for (auto i = data.begin(); i != data.end(); i++)
+		{
+			cout << *i<< " ";
+		}
+	}
+};
