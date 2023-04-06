52990af7f3589bc5373b5eb9f41890f0e85c77bc
Nathan <xdnathanm@gmail.com>
2023-04-05 20:35:52 -0600

    added comments

--- Sets/Sets.cpp
+++ Sets/Sets.cpp
@@ -1,32 +1,32 @@
 #include <Sets.h>
 
-
+// default constructor
 set :: set()
 {
 	 vector < int> setList;
 }
-
+// deconstructor
 set::~set()
 {
 	setList.clear();
 }
-
+// addes an element to a set
 void set::operator+=(int element)
 {
-	// TODO: insert return statement here
+	
 	setList.push_back(element);
 
 
 }
-
+// removes an element from a set
 void set::operator-=( set& nSet)
 {
-	// TODO: insert return statement here
+	
 	nSet.setList.pop_back();
 	
 }
 
-void set::print()
+void set::print()  // prints the sets
 {
 	cout << " elements from set: ";
 	for (auto i = setList.begin(); i != setList.end(); i++)
@@ -36,59 +36,61 @@ void set::print()
 	cout << endl;
 }
 
-set operator*(const set& nSet1, const set& nSet2)
+set operator*(const set& nSet1, const set& nSet2)  // intersection set command
 {
 	
-	set intersect;
+	set intersect;  // creates the return value for intersect
 	
 	//iterators 
 	int j = 0;
 	int l = 0;
-	auto i = nSet1.setList.begin();
 	
- 	for (i; i != nSet1.setList.end(); i++)
+	
+ 	for (auto i = nSet1.setList.begin(); i != nSet1.setList.end(); i++)			// loop for first arguemnt
 	{
-		for (auto k = nSet2.setList.begin(); k != nSet2.setList.end(); k++)
+		for (auto k = nSet2.setList.begin(); k != nSet2.setList.end(); k++)		// loop for second argument 
 		{
-			if (nSet1.setList.at(j) == nSet2.setList.at(l))                        // vector out of range error.
+			if (nSet1.setList.at(j) == nSet2.setList.at(l))                     // check if the values at the current locations are equal
 			{
 				
-				intersect.setList.push_back(nSet2.setList.at(l));
+				intersect.setList.push_back(nSet2.setList.at(l));				// stores the value in the return set
 				
 			}
 			j++;
 		}
-		j = 0;
+		j = 0;																	// resets iterator for inner loop
 		l++;
 	}
 	return intersect;
 }
 
-set operator+( const set& nSet1, const set& nSet2 )
+set operator+( const set& nSet1, const set& nSet2 )   // union set command
 {
-	set unions;
-	int j = 0;
+	set unions; // creates the return set
+
+	int j = 0;	// iterators
 	int l = 0;
-	int sI = 0;
+
+	int sI = 0;  // used to store the location in the second argument set. 
 	bool same = false;
-	for (auto i = nSet1.setList.begin(); i != nSet1.setList.end(); i++)
+	for (auto i = nSet1.setList.begin(); i != nSet1.setList.end(); i++)	// first argument loop
 	{
-		for (auto k = nSet2.setList.begin(); k != nSet2.setList.end(); k++)
+		for (auto k = nSet2.setList.begin(); k != nSet2.setList.end(); k++) // second argument loop
 		{
-			if (nSet1.setList.at(l) == nSet2.setList.at(j))
+			if (nSet1.setList.at(l) == nSet2.setList.at(j))   // check if the value in set one at a specified location is the same as a value in set2
 			{
-				same = true;
-				sI = j;
+				same = true;								// sets bool check to true
+				sI = j;										// saves location for the same value in set2
 			}
 			j++;
 		}
-		if (same)
+		if (same)	// only stores the value from set2 at a SI location
 		{
 			
-			unions.setList.push_back(nSet2.setList.at(sI));
+			unions.setList.push_back(nSet2.setList.at(sI));	
 			same = false;
 		}
-		else
+		else // stores both the values from set1 and set2
 		{
 			unions.setList.push_back(nSet1.setList.at(l));
 			unions.setList.push_back(nSet2.setList.at(l));
@@ -103,32 +105,32 @@ set operator+( const set& nSet1, const set& nSet2 )
 	return unions;
 }
 
-set operator-(const set& nSet1, const set& nSet2)
+set operator-(const set& nSet1, const set& nSet2)  // differance set command
 {
-	set differance;
-	int j = 0;
+	set differance; // return set
+	int j = 0; // ittirators
 	int l = 0;
-	int sI = 0;
-	bool same = false;
-	for (auto i = nSet1.setList.begin(); i != nSet1.setList.end(); i++)
+	
+	bool same = false;  // bool used for check when the numbers are the same
+	for (auto i = nSet1.setList.begin(); i != nSet1.setList.end(); i++) // loops through the first argument
 	{
-		for (auto k = nSet2.setList.begin(); k != nSet2.setList.end(); k++)
+		for (auto k = nSet2.setList.begin(); k != nSet2.setList.end(); k++) // loops through the second arguemnt
 		{
-			if (nSet1.setList.at(l) == nSet2.setList.at(j))
+			if (nSet1.setList.at(l) == nSet2.setList.at(j)) // checks to see if the numbers are the same so they are excluded
 			{
 				same = true;
-				sI = j;
+				
 			}
 			j++;
 		}
 		if (!same)
 		{
 
-			differance.setList.push_back(nSet1.setList.at(l));
+			differance.setList.push_back(nSet1.setList.at(l)); // if the numbers are not the same they stored in the return set
 			same = false;
 		}
-		same = false;
-		j = 0;
+		same = false;	// resets same to be false for the next numbers
+		j = 0;			// resets the iterator for the second argument
 		l++;
 
 	}
--- Sets/TestSets.cpp
+++ Sets/TestSets.cpp
@@ -5,27 +5,33 @@
 int main()
 {
 	std:: cout << std::endl;
-	set set1 , set2, set3;
-	int choice = 0;
-	char input;
-	set1 += 1;
+	set set1 , set2, set3;  // creates an instance of the 3 sets that will be used
+	int choice = 0;         // used for a better UI
+	char input;				// used for a better UI
+
+	set1 += 1;				// filling the sets with random numbers with atlest 2 the same
 	set1 += 5;
 	set1 += 4;
 	set1 += 7;
 	set1 += 8;
+	set1 += 10;
 
 	set2 += 6;
 	set2 += 3;
 	set2 += 4;
 	set2 += 7;
 	set2 += 9;
+	set2 += 6;
+						// removes the last two numbers
+	set1 -= set1;
+	set2 -= set2;
 
 	set1.print();
 	set2.print();
 
 	std:: cout << std ::endl << "would you like to run a set Command? (y/n)" << std ::endl;
 	std::cin >> input; 
-	while (input != 'n')
+	while (input != 'n')		// used to loop through so the set commands can be used more than once without having to relaunch the cod
 	{
 		std:: cout << std ::endl << std :: endl << " 1) intersection, 2) union, 3) difference " << std ::endl;
 
@@ -53,8 +59,11 @@ int main()
 		std ::cin >> input;
 		input = tolower(input);
 	}
+
+
 	set1.~set();
 	set2.~set();
 	set3.~set();
+	cout << endl;
 	return 0;
 }
